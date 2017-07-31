eac719f2342ddd532196815b7ec55919362d5387
Katy Lion <lnyntng@gmail.com>
2017-07-31 18:54:32 +0200

    Basic implementation of add method in String Calculator

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,4 +1,18 @@
 class StringCalculator {
+	add(numbers){
+		if(numbers === ""){
+			return 0;
+		} else {
+			var separators = [',', '\n'];
+			separators.join('|');
+			var arr = numbers.split(new RegExp(separators.join('|'), 'g'));
+			var sum = 0;
+			for(var i = 0; i < arr.length; i++){
+				sum += parseInt(arr[i]);
+			}
+			return sum;
+		}
+	};
 }
 
 export default StringCalculator;
--- test/string-calculator.test.js
+++ test/string-calculator.test.js
@@ -6,4 +6,36 @@ import {
 const calculator = new StringCalculator();
 
 describe('String Calculator', () => {
+	describe('add', () => {
+		describe('empty string as param', () => {
+			it('should return 0', () => {
+				var result = calculator.add('');
+				expect(result).to.equal(0);
+			});
+		});
+		describe('1 number string as param', () => {
+			it('should return the same number', () => {
+				var result = calculator.add('1');
+				expect(result).to.equal(1);
+			});
+		});
+		describe('two numbers string as param', () => {
+			it('should return the addition of the numbers', () => {
+				var result = calculator.add('1,2');
+				expect(result).to.equal(3);
+			});
+		});
+		describe('many numbers string as param', () => {
+			it('should return the addition of the numbers', () => {
+				var result = calculator.add('1,2,1,1,1,1,1');
+				expect(result).to.equal(8);
+			});
+		});
+		describe('numbers string separated by new space as param', () => {
+			it('should return the addition of the numbers', () => {
+				var result = calculator.add('1\n2,3');
+				expect(result).to.equal(6);
+			});
+		});
+	});
 });
